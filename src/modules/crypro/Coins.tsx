849fd9100d4b5cffac37ee414c1cce352677ad68
Kdsinghak <kuldeep.singh1@appinventiv.com>
2023-04-14 16:23:03 +0530

    check code push

--- src/modules/crypro/Coins.tsx
+++ src/modules/crypro/Coins.tsx
@@ -26,7 +26,7 @@ const Coins = () => {
         flex: 1,
       }}>
       <Text>Coins</Text>
-      <Image source={require('../../assets/add.png')} />
+      <Image source={require('../../assets/edit.png')} />
     </View>
   );
 };
